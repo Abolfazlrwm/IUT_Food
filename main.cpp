0a84a3b8cea2689bd36f1a47018549e7163fa6b3
AmirHaghiy <r6seige1248@gmail.com>
2025-07-01 18:56:38 +0330

    test admin panel

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
-
+#include "adminpanel.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    AdminPanel w;
     w.show();
     return a.exec();
 }
